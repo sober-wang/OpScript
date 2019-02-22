0e19f20e7d9c87348fee55bea59042d83854801c
sober <ws1992jx@163.com>
2019-02-22 22:24:56 +0800

    readLine.go 代码作用和知识点

--- goStudy/readLine.go
+++ goStudy/readLine.go
@@ -1,3 +1,10 @@
+/*
+	os.Args[1] 可以获取命令行参数，os.Args[0] 是执行文件本体
+	defer 在函数执行完成后做的操作，防止忘记关闭链接，关闭文件等，或者在函数执行完成后去做一些其他的操作。比较消耗资源
+	time.Sleep() 默认秒数为纳秒，非常小的一个单位。通常配合 time 中的其他函数使用
+	bufio 库可以实现对文件的操作。在bufio库中可以设置数据读写缓存设置，这样可以对读写进行加速
+
+*/
 package main
 
 import (
@@ -17,6 +24,7 @@ func main(){
 		fmt.Println("This have a ERROR",err)
 		return
 	}
+	defer f.close()
 
 	br := bufio.NewReader(f)
 	for {
